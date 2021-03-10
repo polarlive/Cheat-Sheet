24d789cf4fd41639945dab62d4eac71b0bddd445
polarlive <73319092+polarlive@users.noreply.github.com>
2021-03-10 11:28:37 -0500

    Create Mad Libs.py

---
+++ Mad Libs.py
@@ -0,0 +1,9 @@
+color = input("Enter a color: ")
+name = input("Enter a name: ")
+day_of_the_week = input("Enter a day of the week Ex. Monday-Sunday: ")
+age = input("Enter a number: ")
+
+print("There was a person named " + name)
+print("They wore a " + color + " dress")
+print("They were " + age + " years old")
+print("And it was a " + day_of_the_week + " afternoon")
